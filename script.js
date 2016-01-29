ad3819c26f3679db0aedf676274f28503c03bd6b
Neal Cloud <neal.cloud@gmail.com>
2016-01-29 12:54:59 -0800

    add stats functions - Neal Cloud

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ $(document).ready(function() {
     Data.mortyInit = Game.randomMorty(Data.totalCards/2);
     Game.createMortyMatch(Data.mortyInit);
     //create a click event on all card backs
-    Game.btnMaker();
+    Game.btnBackMaker();
+
+    //create reset btn
+    Game.resetBtn();
 });
 
 //game variable storage
@@ -15,18 +18,19 @@ Data = {
     first_card_clicked: null,
     second_card_clicked: null,
     mortyInit: null,
-    score: 0,
+    matches: 0,
     firstcardId: null,
     gameBoard: null,
     totalCards: 18,
-    attempt: 0,
-    accuracy: 0
+    attempts: 0,
+    accuracy: 0,
+    games_played: 0
 }
 
 //game logic "engine" functions
 Game = {
     //assign clicks to buttons and card backs;
-    btnMaker: function(){
+    btnBackMaker: function(){
         //Card Clicked if second_card_clicked -> card_clicked  else shake card
         $(".back").click(function(){
             //console.log("clicked a" , this);
@@ -38,22 +42,28 @@ Game = {
             }
         })
         //THE RESET BUTTON
+    },
+    resetBtn:function(){
         $(".reset").click(function(){
             //console.log("clicked", this);
             //clear gameboard;
             $("#game-area").html("");
             //checks for premade gameboard otherwise use dynamic function to create
-            Data.gameBoard ? $("#game-area").append(Data.gameBoard) : Data.mortyInit = Game.randomMorty(Data.totalCards/2);
+            Data.mortyInit = Game.randomMorty(Data.totalCards/2);
             //resets the Data vars and initiates game
             Game.createMortyMatch(Data.mortyInit);
-            Data.score = 0;
+            Data.matches = 0;
+            Data.games_played++;
+            Game.reset_stats();
             Game.resetDataFlags();
-            Game.btnMaker();
+            Game.btnBackMaker();
         })
     },
     // called when a card is clicked enacts core game logic
     // arguments used (element, boolean)  targ element card and if 2nd card clicked
     card_clicked: function(targ){
+
+
         //if first_card_clicked is false flip this card and store this element and toggle first_card_clicked to true
         if(!Data.first_card_clicked){
             $(targ).addClass("flip");
@@ -67,6 +77,9 @@ Game = {
     },
    //Second card clicked -> (*mouse click disabled) flip second card and set true and flip target card and compare img src
     compareCards: function(targ){
+        Data.attempts++;
+        Data.accuracy = (Math.floor(Data.matches / Data.attempts) * 100) + "%";
+        Game.display_stats();
         Data.second_card_clicked = true;
         $(targ).addClass("flip");
         //The card src images are not equal -> use Timeout to delay a flip back and reset data variables and wait for click
@@ -84,20 +97,19 @@ Game = {
     },
     matchedCards: function(targ){
         //game is won -> clear game board and display win text and wait for reset
-        if(Data.score == Data.totalCards / 2 - 1 ){
+        if(Data.matches == Data.totalCards / 2 - 1 ){
             Game.animate(targ);
             $("#game-area").html("");
             $("#game-area").append($("<h1>").html("You win").addClass('win'));
         }
-        //not yet won -> increase score, fade out cards and reset data flag variables and wait for next click
+        //not yet won -> increase matches, fade out cards and reset data flag variables and wait for next click
         else{
-            console.log(Data.score, Data.totalCards);
-            Data.score++;
+            console.log(Data.matches, Data.totalCards);
+            Data.matches++;
             Game.animate(targ);
             setTimeout(function(){
                 Game.resetDataFlags();
             }, 500);
-
         }
     },
     //animate an elements sibling
@@ -110,6 +122,18 @@ Game = {
         Data.second_card_clicked = null;
         Data.firstcardid = null;
         Data.first_card_clicked = null;
+    },
+    display_stats: function(){
+        $(".attempts").find(".value").text(Data.attempts);
+        $(".matches").find(".value").text(Data.matches);
+        $(".accuracy").find(".value").text(Data.accuracy);
+        $(".games-played").find(".value").text(Data.games_played);
+    },
+    reset_stats: function(){
+        Data.accuracy = 0;
+        Data.matches = 0;
+        Data.attempts = 0;
+        Game.display_stats();
     },
     //Below is for creating cards dynamically ignore
     //creates and appends 18 divs with html and img src that uses a random order array from randomMorty
