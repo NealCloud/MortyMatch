5168119b98f5547e692af68b50c3dc971a405820
Neal Cloud <neal.cloud@gmail.com>
2016-01-31 19:34:18 -0800

    add portals - NC

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
  */
 $(document).ready(function() {
     // initialize game creating the board game with a randomly generated cardset or
-    //Data.mortyInit = Game.randomMorty(Data.totalCards/2);
-    Game.createMortyMatch();
+    //Data.mortyInit = Game.randomCardArray(Data.totalCards/2);
+   // Game.createMortyMatch();
     //create a click event on all card backs
-    Game.btnBackMaker();
+   // Game.btnBackMaker();
     //create reset btn
-    Game.resetBtn();
+
+    Game.mortyInit();
+
 });
 
 //game variable storage
@@ -19,7 +21,7 @@ Data = {
     mortyInit: null,
     gameBoard: null,
     //Stats
-    world: 0,
+    currentworld: 0,
     matches: 0,
     totalCards: 18,
     attempts: 0,
@@ -27,13 +29,22 @@ Data = {
     games_played: 0,
     //Timers
     gameTimer: 0,
+    timing: false,
     countdown: 0,
+    counting: false,
     time_display: ".timer .value",
     //Items
+    portalReady: true,
+    portalFuel: 10,
     morties_collected: [],
-    morty_list: ["trueMorty","roboMorty","cyclopsMorty","rainbowMorty","businessMorty","psychoMorty","wizardMorty","magicMorty","bikerMorty"],
+    items_collected: [],
+    item_list: ["morty1.png","morty2.png","morty3.png","morty4.png","morty5.png",
+        "morty6.png","morty7.png","morty8.png","morty9.png"],
+    morty_list: ["trueMorty","roboMorty","cyclopsMorty","rainbowMorty","businessMorty",
+        "psychoMorty","wizardMorty","magicMorty","bikerMorty"],
+    world_list: ["world1","world2","world3","world4","world5","world6"],
     morty_dex: {
-        trueMorty: {hp:100, attack: 200},
+        trueMorty: {health:200, stamina: 200, wit: 20, strength:20, speed:20, dead: false},
         roboMorty: {},
         cyclopsMorty: {},
         rainbowMorty: {},
@@ -42,11 +53,24 @@ Data = {
         wizardMorty: {},
         magicMorty: {},
         bikerMorty: {},
+    },
+    worlds: {
+        world0:["<div> its ricks mart!</div>", "world0"],
+        world1: ["<div> its a colorful planet!</div>", "world1"],
+        world2: ["<div> its world2!</div>", "world2"],
+        world3: ["<div>  its world3!</div>", "world3"],
+        world4: ["<div>  its world4!!</div>", "world4"],
+        world5: ["<div>  its world5!!</div>", "world5"],
+        world6: ["<div>  its world6!</div>", "world6"]
     }
 }
-
 //game logic "engine" functions
 Game = {
+    mortyInit: function(){
+        var world = Data.worlds.world0;
+        $("#game-area").addClass(world[1]).append(world[0]);
+        Game.sideBtnMaker();
+    },
     //assign clicks to buttons and card backs;
     btnBackMaker: function(){
         //Card Clicked if second_card_clicked -> card_clicked  else shake card
@@ -61,37 +85,59 @@ Game = {
         })
         //THE RESET BUTTON
     },
-    resetBtn:function(){
+    sideBtnMaker:function(){
         $(".reset").click(function(){
             //console.log("clicked", this);
             //clear gameboard;
             $("#game-area").html("");
             //checks for premade gameboard otherwise use dynamic function to create
-            //reset timer
-            clearInterval(Data.gameTimer);
-            Data.gameTimer = 0;
-            Data.static_time = new Date();
+
             //resets the Data vars and initiates game
             Game.createMortyMatch();
             Data.matches = 0;
-            Data.games_played += Data.attempts > 0? 1: 0;
+            Data.games_played += Data.attempts > 0 ? 1: 0;
             Game.reset_stats();
             Game.resetDataFlags();
             Game.btnBackMaker();
             $('.timer .value').text("0");
-            $("#game-area").removeClass('world1');
-            $("#game-area").addClass('world2');
+        })
+
+        $(".portal").click(function(){
+            if(Data.portalReady && Data.portalFuel > 0){
+                if(Data.portalFuel < 3){
+                    console.log("your running low on fuel!");
+                }
+                Data.portalFuel--;
+                Game.portal(Data.currentworld);
+            }
         })
 
         $(".btnflip").click(function(){
-           Game.flipEverything();
+            Game.flipEverything();
         })
     },
+    portal: function(previousWorld){
+        var world = Data.worlds["world" + previousWorld];
+        $("#game-area").removeClass(world[1]);
+        console.log(previousWorld);
+        var r = previousWorld;
+        while(r == previousWorld){
+           r = Math.floor(Math.random() * Data.world_list.length + 1);
+        }
+        console.log(r, previousWorld);
+        world = Data.worlds["world" + r];
+        console.log(world);
+        Data.currentworld = r;
+        $("#game-area").addClass(world[1]).append(world[0]);
+
+    },
+
     // called when a card is clicked enacts core game logic
     // arguments used (element, boolean)  targ element card and if 2nd card clicked
     card_clicked: function(targ){
-        if (Data.gameTimer == 0) {
-           Game.stopwatch(10, Data.time_display);
+        if (!Data.counting) {
+           Data.counting = true;
+           Game.stopwatch(-10, Data.time_display);
         }
         //if first_card_clicked is false flip this card and store this element and toggle first_card_clicked to true
         if(!Data.first_card_clicked){
@@ -124,7 +170,7 @@ Game = {
         }
         //The cards are equal -> add morty to pokedex and move to matchedCards function to check if game won
         else{
-            var firstNum = /\d+/.exec(firstSrc)[0];
+            var firstNum = /image/.exec(firstSrc)[0];
             Game.addMorty(firstNum);
             Game.matchedCards(card);
         }
@@ -188,42 +234,36 @@ Game = {
         Game.display_stats();
     },
     stopwatch: function(duration, display){
-        var total = duration, timer  = 0, minutes, seconds;
-        Data.countdown = setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
+        var timer, total, minutes, seconds, variable;
+        console.log(duration);
+        if(duration > 0){
+            console.log(variable);
+            variable = "Data.gameTimer";
+            timer = 0;
+            total = duration;
+        }
+        else{
+            variable = "Data.countdown";
+            timer = duration;
+            total = 0;
+        }
+        //console.log(timer);
+        window[variable] = setInterval(function () {
+            console.log(timer);
+            minutes = Math.abs(parseInt(timer / 60, 10));
+            seconds = Math.abs(parseInt(timer % 60, 10));
 
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
 
             $(display).text(minutes + ":" + seconds);
 
-            if (++timer > duration - 5) {
-                if(timer >= duration){
-                    clearInterval(Data.gameTimer);
+            if (++timer > total - 5) {
+                if(timer >= total + 1){
+                    clearInterval(window[variable]);
                     console.log("TIMER OUT");
                 }
-                $(display).addClass("win");
-            }
-        }, 1000);
-    },
-    countdown: function(duration, display){
-        var timer = duration, minutes, seconds;
-        Data.gameTimer = setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-
-            $(display).text(minutes + ":" + seconds);
-
-            if (--timer < 5) {
-                if(timer <= 0){
-                    clearInterval(Data.gameTimer);
-                    console.log("TIMER OUT");
-                }
-                $(display).addClass("win");
+                //$(display).addClass("win");
             }
         }, 1000);
     },
@@ -233,17 +273,16 @@ Game = {
 
         if(cards < 9 && cards > 1){
             console.log(typeof cards, cards);
-            Game.dynamicCardCreate(Game.randomMorty(cards));
+            Game.dynamicCardCreate(Game.randomCardArray(cards, Data.item_list));
         }
         else{
             console.log(Data.totalCards/2);
-            //calls for a dynamic card creation  * use the randomMorty function to create such an array
-            Game.dynamicCardCreate(Game.randomMorty(Data.totalCards / 2));
+            //calls for a dynamic card creation  * use the randomCardArray function to create such an array
+            Game.dynamicCardCreate(Game.randomCardArray(Data.totalCards / 2, Data.item_list));
         }
     },
     //create card divs with img src in the order of a number array
     dynamicCardCreate : function(numberArray){
-
         var numLen = numberArray.length/2;
         console.log(numLen);
         for (var i = 0; i < numLen; i++) {
@@ -251,7 +290,7 @@ Game = {
                 var cardcontain = $("<div>", {
                     class: "card",
                     html: " <div class='front'>" +
-                    "<img src='image/morty" + numberArray.pop() + ".png' alt='cardfront'>" +
+                    "<img src='image/" + numberArray.pop() + "' alt='cardfront'>" +
                     "</div>" +
                     "<div class='back'> " +
                     "<img src='image/cardback.png' alt='cardback'>" +
@@ -261,13 +300,13 @@ Game = {
             }
         }
     },
-    //returns an array of predefined numbers in a random order;
-    randomMorty: function(num){
-        console.log("randomMorty array:", num);
+    //take in an array of images doubles and randomizes them;
+    randomCardArray: function(num, items){
+        console.log("randomCardArray array:", num, items);
         pics = [];
-        for(var i = 1; i < num + 1; i++){
-            pics.push(i);
-            pics.push(i);
+        for(var i = 0; i < num ; i++){
+            pics.push(items[i]);
+            pics.push(items[i]);
         }
         //remove to have random card placement
         //return pics;
@@ -284,7 +323,15 @@ Game = {
     },
     //add Morty to index
     addMorty: function(morty){
-        console.log(morty);
+        console.log(morty + " added");
+        var mortyName = Data.morty_list[morty - 1];
+        console.log(mortyName);
+        Data.morties_collected.push(Data.morty_dex[mortyName]);
+        $("<option>").text(mortyName).appendTo("#mortydex");
+        console.log(Data.morties_collected);
+    },
+    addItem: function(item){
+        console.log(morty + " added");
         var mortyName = Data.morty_list[morty - 1];
         console.log(mortyName);
         Data.morties_collected.push(Data.morty_dex[mortyName]);
